42e26f18050e62904a25fb48cbf5128250b6a72e
alvarososaa <alvarosantgososagarcia@gmail.com>
2021-03-20 19:34:04 +0100

    PRACTICE 1

---
+++ P2/Ex5.py
@@ -0,0 +1,13 @@
+from CLIENT0 import client
+from Seq1 import Seq
+print("---------EXCERSISE 4----------")
+c = Seq()
+seq = c.read_fasta("U5.txt")
+ip = "localhost"
+PORT = 8008
+print("Connection to server with ip: ", ip, "Port: ", PORT)
+c = client(ip, PORT)
+print("To server: Sending the U5.txt sequence ")
+print(c.talk("Sending the U5.txt sequence"))
+print("To server: ", seq)
+print(c.talk(seq))
